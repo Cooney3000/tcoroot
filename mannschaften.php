18febc15e14be46939f37b4b4fe7ef436abd723e
Cooney3000 <conny.roloff@live.de>
2022-05-04 12:47:20 +0200

    .

--- mannschaften.php
+++ mannschaften.php
@@ -11,27 +11,33 @@ include 'header.php';
 			<section id="mannschaften" class="seite">
 				<h2>Die Mannschaften des TC Olching</h2>
 				<article>
-          <p>Für die Mannschaftsführer: <a href="https://www.btv.de/dam/jcr:2b0c670b-2ecd-4b1f-9564-f4f39a0f7e0d/btv_regeln_2019_IV_Wettspielbestimmungen%20(WSB)_200119_mon-2.pdf"> Die Durchführungsbestimmungen 2019</a></p>
+          <p>Für die Mannschaftsführer: <a href="https://www.btv.de/dam/jcr:2b0c670b-2ecd-4b1f-9564-f4f39a0f7e0d/btv_regeln_2022_IV_wettspielbestimmungen%20btv.pdf"> Die Durchführungsbestimmungen 2022</a></p>
           <p><a href="https://www.btv.de/de/mein-verein/vereinsseite/tc-olching.html#Mannschaften">Die Mannschaften des TC Olching beim BTV</a></p>
-          <ul>
-          <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=1742421" target="_blank" rel="noopener">Herren 55 (5er)</a></li>
+          
+          <h3>Direkte Links zu den Mannschaftsportraits der BTV-Website</h3>
+          <p>
+            Da der BTV zum einen eine benutzerunfreundliche Website betreibt, die es Webmastern schwer macht, einzelne Links einfach zu ermitteln und 
+            sich zum anderen die Links zu den einzelnen Mannschaften dieses Jahr bereits mehrfach geändert haben, können wir diesen Service leider nicht mehr anbieten. 
+          </p>
+          
+          <!-- <ul>
+          <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=2765930" target="_blank" rel="noopener">Herren 55 (5er)</a></li>
           <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=1742354" target="_blank" rel="noopener">Herren 50</a></li>
           <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=1742386" target="_blank" rel="noopener">Herren 50 II</a></li>
           <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=1742323" target="_blank" rel="noopener">Herren 40</a></li>
           <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=1742296" target="_blank" rel="noopener">Herren 30 (4er)</a></li>
           <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=1742099" target="_blank" rel="noopener">Herren</a></li>
           <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=1742204" target="_blank" rel="noopener">Damen</a></li>
-          <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=1741118" target="_blank" rel="noopener">Damen 40</a></li>
+          <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=2735323" target="_blank" rel="noopener">Damen 40</a></li>
           <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=1742522" target="_blank" rel="noopener">Damen 50 (4er)</a></li>
-          <!-- <li><a href="https://www.btv.de/de/spielbetrieb/tabelle-spielplan.html?groupid=1742993" target="_blank" rel="noopener">Freizeit-Damen Doppel 60</a></li> -->
-          </ul>
+          </ul> -->
 				</article>
 			</section>
 		</div>
 
 		<div id="blatt2">
 			<section id="platzbelegung" class="seite">
-				<h2>Platzbelegung Sommer 2021</h2>
+				<h2>Platzbelegung Sommer 2022</h2>
 				<p>Der TC Olching verfügt über 6 Sandplätze.</p>
 				<p><strong>BITTE BEACHTE</strong>, dass während der Punktspielzeit die Plätze an vielen Wochenenden belegt sind. 
 				Die Platzbelegungen des Mannschaftstrainings und des Vereinstrainers findest du im folgenden Plan. 
@@ -45,7 +51,7 @@ include 'header.php';
 		</div>
 		<div id="blatt3">
 			<section id="begegnungen" class="seite">
-				<h2>Die Spiele des TC Olching in 2020</h2>
+				<h2>Die Spiele des TC Olching in 2022</h2>
 				<p>Zusätzlich zu den Trainingszeiten gibt es Platzbelegungen an Wochenenden und Feiertagen. Eine Begegnung dauert durchschnittlich 5 Stunden, also von 09:00 bis 14:00 Uhr oder von 14 Uhr bis 19 Uhr, kann sich aber auch länger ziehen. Eine 6er-Mannschaft 
 				belegt i.&nbsp;d.&nbsp;R. immer mindestens 3 Plätze, 
 				eine 4er-Mannschaft mindestens 2. Hier sind die Spieltermine:</p>
@@ -61,7 +67,6 @@ include 'header.php';
           <tr>	<td align="right">	So. 08.05.2022 10:00	</td>	<td>	S3	</td>	<td><strong>	Damen	</strong></td>	<td>	TC Gernlinden II	</td>	</tr>
           <tr>	<td align="right">	14:00	</td>	<td>	S3	</td>	<td><strong>	Herren 50 II	</strong></td>	<td>	TSV Leitershofen	</td>	</tr>
           <tr>	<td align="right">	15:30	</td>	<td>	S2	</td>	<td>	TC Puchheim II		<td><strong>	Dunlop Midcourt U10 (4er)	</strong></td>	</tr>
-          <tr>	<td align="right">	Mo. 09.05.2022 10:00	</td>	<td>	S1	</td>	<td>	TCE Gröbenzell		<td><strong>	Freizeit-Damen Doppel 60	</strong></td>	</tr>
           <tr>	<td align="right">	Fr. 13.05.2022 16:00	</td>	<td>	S3	</td>	<td><strong>	Knaben 15 (4er)	</strong></td>	<td>	Münchner Sportclub III	</td>	</tr>
           <tr>	<td align="right">	16:00	</td>	<td>	S5	</td>	<td>	SV Haspelmoor		<td><strong>	Knaben 15 II (4er)	</strong></td>	</tr>
           <tr>	<td align="right">	Sa. 14.05.2022 09:00	</td>	<td>	S3	</td>	<td><strong>	Juniorinnen 18 (4er)	</strong></td>	<td>	TC Gernlinden	</td>	</tr>
@@ -74,7 +79,6 @@ include 'header.php';
           <tr>	<td align="right">	So. 15.05.2022 10:00	</td>	<td>	S4	</td>	<td><strong>	Herren	</strong></td>	<td>	TC Lauterbach 1973 II	</td>	</tr>
           <tr>	<td align="right">	10:00	</td>	<td>	S3	</td>	<td>	TSV Türkenfeld		<td><strong>	Damen	</strong></td>	</tr>
           <tr>	<td align="right">	10:00	</td>	<td>	S4	</td>	<td>	TC Landsberied		<td><strong>	Herren 30 (4er)	</strong></td>	</tr>
-          <tr>	<td align="right">	Mo. 16.05.2022 10:00	</td>	<td>	S1	</td>	<td><strong>	Freizeit-Damen Doppel 60	</strong></td>	<td>	SV Lochhausen	</td>	</tr>
           <tr>	<td align="right">	Fr. 20.05.2022 16:00	</td>	<td>	S3	</td>	<td>	SV Lochhausen		<td><strong>	Knaben 15 (4er)	</strong></td>	</tr>
           <tr>	<td align="right">	16:00	</td>	<td>	S5	</td>	<td><strong>	Knaben 15 II (4er)	</strong></td>	<td>	TC Gernlinden III	</td>	</tr>
           <tr>	<td align="right">	Sa. 21.05.2022 09:00	</td>	<td>	S3	</td>	<td>	TF Dachau		<td><strong>	Juniorinnen 18 (4er)	</strong></td>	</tr>
@@ -88,7 +92,6 @@ include 'header.php';
           <tr>	<td align="right">	10:00	</td>	<td>	S3	</td>	<td>	MTV Berg		<td><strong>	Damen	</strong></td>	</tr>
           <tr>	<td align="right">	14:00	</td>	<td>	S2	</td>	<td><strong>	Herren 40	</strong></td>	<td>	SV Weichs	</td>	</tr>
           <tr>	<td align="right">	15:30	</td>	<td>	S2	</td>	<td><strong>	Dunlop Midcourt U10 (4er)	</strong></td>	<td>	TG Germerswang	</td>	</tr>
-          <tr>	<td align="right">	Mo. 23.05.2022 10:00	</td>	<td>	S1	</td>	<td>	TC Gernlinden		<td><strong>	Freizeit-Damen Doppel 60	</strong></td>	</tr>
           <tr>	<td align="right">	Do. 26.05.2022 10:00	</td>	<td>	LL2	</td>	<td>	Damen 40		<td>	TSV Oberbeuren	</td>	</tr>
           <tr>	<td align="right">	Sa. 28.05.2022 09:00	</td>	<td>	S3	</td>	<td>	TSV Eintracht Karlsfeld		<td><strong>	Juniorinnen 18 (4er)	</strong></td>	</tr>
           <tr>	<td align="right">	09:00	</td>	<td>	S3	</td>	<td><strong>	Junioren 18 (4er)	</strong></td>	<td>	TSV Moosach München	</td>	</tr>
@@ -102,7 +105,6 @@ include 'header.php';
           <tr>	<td align="right">	So. 29.05.2022 10:00	</td>	<td>	S4	</td>	<td>	TC Gernlinden IV		<td><strong>	Herren	</strong></td>	</tr>
           <tr>	<td align="right">	10:00	</td>	<td>	LL2	</td>	<td>	TC Murnau		<td>	Damen 40	</td>	</tr>
           <tr>	<td align="right">	15:30	</td>	<td>	S2	</td>	<td>	1. SC Gröbenzell		<td><strong>	Dunlop Midcourt U10 (4er)	</strong></td>	</tr>
-          <tr>	<td align="right">	Mo. 30.05.2022 10:00	</td>	<td>	S1	</td>	<td>	SC Oberweikertshofen		<td><strong>	Freizeit-Damen Doppel 60	</strong></td>	</tr>
           <tr>	<td align="right">	Mo. 20.06.2022 10:00	</td>	<td>	S1	</td>	<td><strong>	Freizeit-Damen Doppel 60	</strong></td>	<td>	TC Eichenau	</td>	</tr>
           <tr>	<td align="right">	Fr. 24.06.2022 16:00	</td>	<td>	S3	</td>	<td><strong>	Knaben 15 (4er)	</strong></td>	<td>	Ausstellungspark München II	</td>	</tr>
           <tr>	<td align="right">	16:00	</td>	<td>	S5	</td>	<td>	TSV Moorenweis		<td><strong>	Knaben 15 II (4er)	</strong></td>	</tr>
@@ -157,9 +159,7 @@ include 'header.php';
 				<h2>Links zu wichtige Tennisregeln</h2>
 				<p>Wenn es mal zu Diskussionen kommt:</p>
 				<ul>
-          <li><a href="https://www.btv.de/dam/jcr:2b0c670b-2ecd-4b1f-9564-f4f39a0f7e0d/btv_regeln_2019_IV_Wettspielbestimmungen%20(WSB)_200119_mon-2.pdf">Regeln / Ordnungen 2019 (BTV)</a></li>
-					<li><a href="https://www.dtb-tennis.de/content/download/12022/129045/version/2/file/Information+zum+Spielen+ohne+Schiedsrichter+15032017.pdf">Spielen ohne Schiedsrichter (DTB)</a></li>
-          <li><a href="https://www.btv.de/de/angebote/fuer-stuhl-und-oberschiedsrichter/informationen-zum-schiedsrichterwesen.html#home-downloads">Wichtige Merkblätter (BTV)</a></li>
+          <li><a href="https://www.btv.de/de/der-btv/ueber-den-btv/regeln-und-ordnungen.html">Regeln / Ordnungen 2022 (BTV)</a></li>
 					<li><a href="http://www.dtb-tennis.de/content/download/680/8523/version/3/file/Tennisregeln.pdf">Tennisregeln (ITF)</a></li>
 				</ul>
 			</section>
